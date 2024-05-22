d870601071f7fd17e55da4839858bf7af2a091db
Aytan <aytangurbanova05@gmail.com>
2024-05-22 09:06:58 +0300

    Update GameConfiguration.java

--- src/main/java/com/omurefeguclu/puzzlegamehw/GameConfiguration.java
+++ src/main/java/com/omurefeguclu/puzzlegamehw/GameConfiguration.java
@@ -3,6 +3,6 @@ package com.omurefeguclu.puzzlegamehw;
 public class GameConfiguration {
     public static final int[] NUMBER_PUZZLE_SIZES = {3, 4};
 
-    public static final int PUZZLE_TIME =5;
+    public static final int PUZZLE_TIME =120;
     public static final String[] IMAGE_PUZZLE_FILES = {"puzzles/image-puzzle-1.jpg", "puzzles/kedo.png"};
 }
