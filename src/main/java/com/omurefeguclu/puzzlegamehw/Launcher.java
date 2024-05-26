d012e9343fc58e5cc49930bd991b563f27623784
Ömür Efe Güçlü <omurefeguclu2004@gmail.com>
2024-05-26 15:21:57 +0300

    .

---
+++ src/main/java/com/omurefeguclu/puzzlegamehw/Launcher.java
@@ -0,0 +1,9 @@
+package com.omurefeguclu.puzzlegamehw;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(PuzzleGameApplication.class, args);
+    }
+}
